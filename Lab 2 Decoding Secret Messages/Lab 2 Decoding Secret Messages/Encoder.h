e222f4f8342578485e0f2d9127d1ce5ff0ebcb26
Michael Cottrell <runksmania1011@gmail.com>
2018-02-05 10:01:22 -0800

    Added Random Encoding to: ENCODER_H

--- Lab 2 Decoding Secret Messages/Lab 2 Decoding Secret Messages/Encoder.h
+++ Lab 2 Decoding Secret Messages/Lab 2 Decoding Secret Messages/Encoder.h
@@ -3,6 +3,8 @@
 #define ENCODER_H
 
 #include "PpmDocument.h"
+#include <cstdlib>
+#include <time.h>
 
 using namespace std;
 
@@ -13,56 +15,53 @@ public:
 	{
 		string message = message_to_encode;
 		int counter = message.size() - 1;
-		int prev_x_coord, prev_y_coord, message_int;
-		int i = doc.getHeight() - 1;
+		int prev_x_coord, prev_y_coord, rand_x_coord, rand_y_coord;
+		int height = doc.getHeight();
+		int width = doc.getWidth();
 		Pixel pixel_to_encode = doc.getPixel(0,0);
+
+		srand(time(NULL));
 
 		while(counter > -1)
 		{
-			for (int j = doc.getWidth() - 1; j > -1; j--)
+			if (counter == 0)
 			{
-				if (counter == 0)
+				pixel_to_encode = doc.getPixel(0, 0);
+				pixel_to_encode.setRed(message[counter]);
+				pixel_to_encode.setGreen(prev_y_coord);
+				pixel_to_encode.setBlue(prev_x_coord);
+				doc.setPixel(0, 0, pixel_to_encode);
+				counter--;
+				break;
+			}
+			else
+			{
+				rand_x_coord = rand() % width;
+				rand_y_coord = rand() % height;
+
+				if (counter == message.size() - 1)
 				{
-					message_int = message[counter];
-					pixel_to_encode = doc.getPixel(0, 0);
-					pixel_to_encode.setRed(message_int);
+					pixel_to_encode = doc.getPixel(rand_y_coord, rand_x_coord);
+					pixel_to_encode.setRed(message[counter]);
+					pixel_to_encode.setGreen(0);
+					pixel_to_encode.setBlue(0);
+					doc.setPixel(rand_y_coord, rand_x_coord, pixel_to_encode);
+					prev_x_coord = rand_x_coord;
+					prev_y_coord = rand_y_coord;
+					counter--;
+				}
+				else
+				{
+					pixel_to_encode = doc.getPixel(rand_y_coord, rand_x_coord);
+					pixel_to_encode.setRed(message[counter]);
 					pixel_to_encode.setGreen(prev_y_coord);
 					pixel_to_encode.setBlue(prev_x_coord);
-					doc.setPixel(0, 0, pixel_to_encode);
+					doc.setPixel(rand_y_coord, rand_x_coord, pixel_to_encode);
+					prev_x_coord = rand_x_coord;
+					prev_y_coord = rand_y_coord;
 					counter--;
-					break;
 				}
-				else if ((counter > 0 ) && ((i + j) % 3 == 0))
-				{
-					if (counter == message.size() - 1)
-					{
-						message_int = message[counter];
-						pixel_to_encode = doc.getPixel(i, j);
-						pixel_to_encode.setRed(message_int);
-						pixel_to_encode.setGreen(0);
-						pixel_to_encode.setBlue(0);
-						doc.setPixel(i, j, pixel_to_encode);
-						prev_x_coord = j;
-						prev_y_coord = i;
-						counter--;
-					}
-					else
-					{
-						message_int = message[counter];
-						pixel_to_encode = doc.getPixel(i, j);
-						pixel_to_encode.setRed(message_int);
-						pixel_to_encode.setGreen(prev_y_coord);
-						pixel_to_encode.setBlue(prev_x_coord);
-						doc.setPixel(i, j, pixel_to_encode);
-						prev_x_coord = j;
-						prev_y_coord = i;
-						counter--;
-					}
-				}
-
 			}
-			
-			i--;
 		}
 
 	}
