5507f2d2b0885fcb376235fa61cc32d8c5193cd1
Michael Cottrell <runksmania1011@gmail.com>
2018-02-06 23:43:22 -0800

    Updated solution to lab 3.  Still not finished.

--- Lab3/Lab3/main.cpp
+++ Lab3/Lab3/main.cpp
@@ -4,10 +4,11 @@
 #include <string>
 #include <stack>
 
-using namespace std;
-
-bool isOperator(string possible_operator);
 double evaluatePostFixExpression(string postfix_string);
+bool isOperator(string possible_operator);
+double performOperation(double left_num, double right_num, string operator_to_operate);
+
+using namespace std;
 
 int main(void) 
 {
@@ -27,29 +28,56 @@ int main(void)
 		{
 			string current_line;
 			getline(input_file, current_line);
-			//vector<string> pieces = StringSplitter::split(current_line, " ");
+
 			double result = evaluatePostFixExpression(current_line);
-			cout << current_line << " = " << result << endl;
+
+			if (result == -1)
+			{
+				string invalid = "Invalid State. Letters are part of computation.";
+				cout << current_line << " = " << invalid << endl;
+			}
+			else if (result == -2)
+			{
+				string invalid = "Invalid State. Not enough numbers to perform calculation.";
+				cout << current_line << " = " << invalid << endl;
+			}
+			else
+			{
+				cout << current_line << " = " << result << endl;
+			}
 		}
 	}
 }
 
 double evaluatePostFixExpression(string postfix_string)
 {
-	double result = -1;
+	double result = 0;
 	stack<double> postfix_stack{};
 	vector<string> pieces = StringSplitter::split(postfix_string, " ");
 
-	//TODO: evaluate pieces as a postfix expression.
 	for (auto piece : pieces)
 	{
-		double right_num;
-		double left_num;
-
-		if (isOperator(piece))
+		if (1 == 0)
 		{
-			right_num = postfix_stack.top();
-			left_num = postfix_stack.top();
+			return -1;
+		}
+		else if (isOperator(piece))
+		{
+			if (postfix_stack.size() < 2)
+			{
+				return -2;
+			}
+			else
+			{
+				double right_num = postfix_stack.top();
+				postfix_stack.pop();
+				
+				double left_num = postfix_stack.top();
+				postfix_stack.pop();
+
+				double operation_result = performOperation(left_num, right_num, piece);
+				postfix_stack.push(operation_result);
+			}
 		}
 		else
 		{
@@ -57,14 +85,16 @@ double evaluatePostFixExpression(string postfix_string)
 		}
 	}
 
-	/*while (postfix_stack.empty() == false)
+	if (postfix_stack.size() == 1)
 	{
-	//in STL, we use top() to get the top item, pop() returns nothing
-	cout << postfix_stack.top();
-
-	//pop is only used to remove the top-most item from the stack
-	postfix_stack.pop();
-	}*/
+		result = postfix_stack.top();
+		postfix_stack.pop();
+		return result;
+	}
+	else
+	{
+		return -2;
+	}
 
 	return result;
 }
@@ -88,7 +118,7 @@ bool isOperator(string possible_operator)
 		is_operator = true;
 		return is_operator;
 	}
-	if (possible_operator == "/")
+	else if (possible_operator == "/")
 	{
 		is_operator = true;
 		return is_operator;
@@ -98,4 +128,31 @@ bool isOperator(string possible_operator)
 		is_operator = false;
 		return is_operator;
 	}
+}
+
+double performOperation(double left_num, double right_num, string operator_to_operate)
+{
+	double result;
+
+	if (operator_to_operate == "+")
+	{
+		result = left_num + right_num;
+		return result;
+	}
+	else if (operator_to_operate == "-")
+	{
+		result = left_num - right_num;
+		return result;
+	}
+	else if (operator_to_operate == "*")
+	{
+		result = left_num * right_num;
+		return result;
+	}
+	else if (operator_to_operate == "/")
+	{
+		result = left_num / right_num;
+		return result;
+	}
+	return 0;
 }
