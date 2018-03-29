40c560dfb17159fb11f4f8a26dacf313a2885f5c
Michael Cottrell <runksmania1011@gmail.com>
2018-03-28 18:41:49 -0700

    Added comments to code. Changed how saving works, and loading.  You can now start a new game or load from a file.  Changed some of the GuessingGame so that it would work with new games.

--- PA4/PA4/GuessingGame.h
+++ PA4/PA4/GuessingGame.h
@@ -1,3 +1,13 @@
+/*
+Assignment: PA
+Description: Create a 20 questions style guessing game.
+Author: Michael Cottrell
+HSU ID: 946839472
+Completion Time: 6 hours.
+In completing this program, I received help from the following people:
+N/A
+*/
+
 #pragma once
 #ifndef GUESSING_GAME_H
 #define GUESSING_GAME_H
@@ -19,6 +29,7 @@ public:
     ~GuessingGame();
     void beginGame();
     void setGameTree(BinaryGuessTree *new_game_tree);
+    void setGameTree(string game_file);
 };
 
 #endif // !GUESSING_GAME_H
