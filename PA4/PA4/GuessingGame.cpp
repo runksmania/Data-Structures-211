304d20e078153550e1ff3e3aae393f3eb45eabbb
Michael Cottrell <runksmania1011@gmail.com>
2018-03-29 19:59:14 -0700

    Added comments to code to clarify how solution was coded.  Also updated documentation to better reflect amount of hours coding solution.

--- PA4/PA4/GuessingGame.cpp
+++ PA4/PA4/GuessingGame.cpp
@@ -3,7 +3,7 @@ Assignment: PA
 Description: Create a 20 questions style guessing game.
 Author: Michael Cottrell
 HSU ID: 946839472
-Completion Time: 6 hours.
+Completion Time: 8 hours.
 In completing this program, I received help from the following people:
 N/A
 */
